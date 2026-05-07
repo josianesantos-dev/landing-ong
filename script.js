b536a39575d58c35becab7ed0deb2e96900a1731
Josiane Santos <josianefds1@gmail.com>
2026-05-07 19:40:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,42 @@ function copiarPix() {
   const pix = document.getElementById("pixKey").innerText;
 
   navigator.clipboard.writeText(pix).then(() => {
-    alert("Chave PIX copiada com sucesso!");
+    alert("PIX copiado!");
   });
 }
 
-/* CARROSSEL */
+/* CARROSSEL COVERFLOW SIMPLES */
 let index = 0;
 const slides = document.querySelectorAll(".slide");
 
-function mostrarSlide() {
+function atualizarCarrossel() {
   slides.forEach(s => s.classList.remove("active"));
+
   slides[index].classList.add("active");
 
   index++;
   if (index >= slides.length) index = 0;
 }
 
-if (slides.length > 0) {
-  setInterval(mostrarSlide, 3000);
+setInterval(atualizarCarrossel, 2500);
+
+/* POPUP DE DOAÇÃO */
+const mensagens = [
+  "❤️ Ana acabou de doar R$20",
+  "💛 Maria ajudou uma família",
+  "✨ Novo apoio recebido"
+];
+
+function mostrarPopup() {
+  const popup = document.getElementById("popup");
+  const msg = mensagens[Math.floor(Math.random() * mensagens.length)];
+
+  popup.innerText = msg;
+  popup.style.opacity = "1";
+
+  setTimeout(() => {
+    popup.style.opacity = "0";
+  }, 3000);
 }
+
+setInterval(mostrarPopup, 5000);
