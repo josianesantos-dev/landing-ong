355d67812d369faefc6cc31a54eaacac5510350e
Josiane Santos <josianefds1@gmail.com>
2026-05-07 19:38:29 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,13 @@ let index = 0;
 const slides = document.querySelectorAll(".slide");
 
 function mostrarSlide() {
-  slides.forEach((s) => s.classList.remove("active"));
-
+  slides.forEach(s => s.classList.remove("active"));
   slides[index].classList.add("active");
 
   index++;
-
-  if (index >= slides.length) {
-    index = 0;
-  }
+  if (index >= slides.length) index = 0;
 }
 
 if (slides.length > 0) {
-  mostrarSlide();
-  setInterval(mostrarSlide, 2500);
+  setInterval(mostrarSlide, 3000);
 }
